9f420d2339c59739833a3cc3b97bb5738bf4340a
Onion <90483907+Anphyschem@users.noreply.github.com>
2025-04-14 18:32:01 +0300

    Изменен файл forms.py
    унифицирована форма ошибки для ввода при добавлении чая

--- tea_project/teas/forms.py
+++ tea_project/teas/forms.py
@@ -1,17 +1,19 @@
+from wsgiref.validate import validator
 from django import forms
 from .models import Tea
 from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
 
 class Tea_form(forms.ModelForm):
     water_temperature = forms.IntegerField(
         label='Температура воды (°C)',
-        validators=[MinValueValidator(20)],
-        error_messages={'min_value': 'Температура не может быть меньше 20°C'} 
+        validators=[MinValueValidator(20), MaxValueValidator(100)],
+        error_messages={'min_value': 'Температура не может быть меньше 20°C', 'max_value': 'Температура не может быть больше 100°C'} 
     )
     spillage_number = forms.IntegerField(
         label='Число проливов',
-        validators=[MinValueValidator(1)],
-        error_messages={'min_value': 'Число проливов не может быть меньше одного!'}
+        validators=[MinValueValidator(1), MaxValueValidator(25)],
+        error_messages={'min_value': 'Число проливов не может быть меньше одного!', 'max_value': 'Слишком много проливов, это уже просто кипяток!'}
     )
     class Meta:
         model = Tea
